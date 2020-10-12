8183b5dcc060461a552dfb640c71e28dd7a411bf
Selena Siri <selenasiri@gmail.com>
2020-10-12 13:23:20 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-<Router>
-  <Fragment>
+<Provider store={store}>
+  <Router>
+   <Fragment>
     <Navbar />
     <Route exact path="/" component={Landing} />
     <section className="container">
@@ -17,8 +22,9 @@ const App = () => (
       <route exact path="/login" component={Login} />
     </switch>
     </section>
-  </Fragment>
-</Router>
+    </Fragment>
+  </Router>
+</Provider>
 )
 
 export default App;
---
+++ src/reducers/alert.js
@@ -0,0 +1,17 @@
+//function that takes a state and action (will be dispatched from an actions file)
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+const initialState = []
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state .filter(alert => alert.id !== payload);
+    default:
+      return state;
+  }
+}
