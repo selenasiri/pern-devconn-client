a3f2839ab61eee0b1ddaaf6c46f64ec858d718cc
Selena Siri <selenasiri@gmail.com>
2020-10-14 15:12:15 -0400

    auth reducer

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL'; // Now dealing with backend HTTP (axios) requests
 //make a request, get response, and render success or fail
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
