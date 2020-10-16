7913487a19af16795ac1fdba0e21358c37d02f67
Selena Siri <selenasiri@gmail.com>
2020-10-16 16:08:35 -0400

    dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,27 +1,38 @@
 import React, { useEffect } from 'react'
-import PropTypes from 'prop-types'
-import { getCurrentProfile } from '../../actions/profile'
-// import { connect } from 'react-redux';
-// import { getCurrentProfile} from '../../action/profile';
+import { Link } from 'react-router-dom';
+import DashboardActions from './DashboardActions'
+import { useSelector, useDispatch } from 'react-redux';
+import { getCurrentProfile} from '../../actions/profile';
+import Spinner from '../layout/Spinner'
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = () => {
+  const { user } = useSelector(state => state.auth)
+  const { profile, loading} = useSelector(state => state.profile)
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    getCurrentProfile();
+    dispatch(getCurrentProfile());
   }, []);
 
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
-
-// Dashboard.propTypes = {
-//   getCurrentProfile: PropTypes.func.isRequired,
-//   auth: PropTypes.object.isRequired,
-//   profile: PropTypes.object.isRequired,
-// };
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <> 
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+          <DashboardActions />
+      ) : (
+        <>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>Create Profile</Link>
+        </> 
+      )}
+    </>
+  );
+};
 
 export default Dashboard
 
-//get the profile state, get profile actions, 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,63 +1,65 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../actions/auth'
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../actions/auth';
 
 const Navbar = () => {
+  const { isAuthenticated, loading } = useSelector(state => state.auth)
   const dispatch = useDispatch()
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-  const loading = useSelector(state => state.auth.loading)
 
   const onLogout = () => {
     dispatch(logout())
   }
 
-
-
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={onLogout} href='#!'>
-          <i className="fas fa-signout-alt"></i>{' '}
-          <span className="hide-sm">Logout</span>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
         </a>
-        </li>
-
-      <li>
-        <Link to="/register">Register</Link>
-        </li>
-      <li>
-        <Link to="/login">Login</Link>
-        </li>
+      </li>
     </ul>
-    )
+  );
 
   const guestLinks = (
     <ul>
       <li>
-        <Link to="!#">Developers</Link>
-        </li>
-
+        <Link to='/profiles'>Developers</Link>
+      </li>
       <li>
-        <Link to="/register">Register</Link>
-        </li>
+        <Link to='/register'>Register</Link>
+      </li>
       <li>
-        <Link to="/login">Login</Link>
-        </li>
+        <Link to='/login'>Login</Link>
+      </li>
     </ul>
-    )
+  );
 
   return (
-    <nav className="navbar bg-dark">
+    <nav className='navbar bg-dark'>
       <h1>
-        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+        <Link to='/'>
+          <i className='fas fa-code' /> DevConnector
+        </Link>
       </h1>
-
-      {!loading && (<>{isAuthenticated ? authLinks : guestLinks }  </>)}
-
+      {!loading && (
+        <>{isAuthenticated ? authLinks : guestLinks}</>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar;
-
+export default Navbar
