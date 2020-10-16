8cca0afa3cbac28fbec9423791426cff77942fe7
Selena Siri <selenasiri@gmail.com>
2020-10-16 17:24:00 -0400

    social

--- src/components/profile-forms/CreateProfile.js
+++ src/components/profile-forms/CreateProfile.js
@@ -18,6 +18,8 @@ const CreateProfile = () => {
     instagram: ''
   });
 
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
   const {
     //put default values and fields here:
     company,
@@ -104,13 +106,14 @@ const CreateProfile = () => {
         </div>
 
         <div className="my-2">
-          <button type="button" className="btn btn-light">
+          <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
 
-        <div className="form-group social-input">
+        {displaySocialInputs && <Fragment>
+          <div className="form-group social-input">
           <i className="fab fa-twitter fa-2x"></i>
           <input type="text" placeholder="Twitter URL" name="twitter" />
         </div>
@@ -134,6 +137,9 @@ const CreateProfile = () => {
           <i className="fab fa-instagram fa-2x"></i>
           <input type="text" placeholder="Instagram URL" name="instagram" />
         </div>
+          </Fragment>}
+
+        
         <input type="submit" className="btn btn-primary my-1" />
         <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
       </form>
