b120b034813c0a7582376bafa8a068828d92a56e
Selena Siri <selenasiri@gmail.com>
2020-10-19 13:36:50 -0400

    postitem

---
+++ src/components/posts/PostItem.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment' // adjusting the date
+import { connect } from 'react-redux' //add like, remove like, delete post
+
+const PostItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostItem
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -2,19 +2,25 @@ import React, { Fragment, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner'
-
+import PostItem from './PostItem'
 
 const Posts = ({ getPosts, post: { posts, loading }}) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
+  return loading ? <Spinner /> : <Fragment>test
+  <h1 className="large text-primary">Posts</h1>
+   <p className="lead">
+    <i className="fas fa-user">Welcome to the community</i>
+  </p>
+  {/* PostForm */}
+  <div>
+    {posts.map(post => (
+      <PostItem key={post._id} post={post} />
+    ))}
+  </div>
+  </Fragment>;
+};
 
 export default Posts
