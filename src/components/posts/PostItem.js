73f185a028aac14d47b4bda1af161b9984b342fe
Selena Siri <selenasiri@gmail.com>
2020-10-19 22:02:47 -0400

    changes

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment' // adjusting the date
 import { connect } from 'react-redux' //add like, remove like, delete post
+import { addLike, removeLike } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }
+const PostItem = ({ addLike, removeLike, auth, post: { _id, text, name, avatar, user, likes, comments, date }
 }) => <div class="post bg-white p-1 my-1">
     <div>
       <Link to="profile.html">
@@ -22,14 +23,14 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
        <p class="post-date">
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-      <button type="button" class="btn btn-light">
+      <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
         <i class="fas fa-thumbs-up"></i>{' '}
         <span>{likes.length > 0 && (
           <span>{likes.length}</span>
         )}</span>
         <span>4</span>
       </button>
-      <button type="button" class="btn btn-light">
+      <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
         <i class="fas fa-thumbs-down"></i>
       </button>
       <Link to={`/post/$_id`} class="btn btn-primary">
