2ed156e45cb0f8d7eabe64b0e52ee104d9e92059
Selena Siri <selenasiri@gmail.com>
2020-10-20 20:48:34 -0400

    post item

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
 import {useParams} from 'react-router-dom'
 
@@ -14,11 +16,13 @@ const Post = ({ getPosts }) => {
     getPost(id);
   }, [getPost, id]);
 
-  return (
-    <div>
-      post
-    </div>
-  )
+  return loading || post === null ? <Spinner /> : 
+  <Fragment>
+    <Link to='/posts' className='btn'> 
+     Back To Posts
+    </Link>
+    <PostItem post={post} showActions={false} /> 
+  </Fragment>
 }
 
 export default Post
--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -4,7 +4,13 @@ import Moment from 'react-moment' // adjusting the date
 import { connect } from 'react-redux' //add like, remove like, delete post
 import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, name, avatar, user, likes, comments, date }
+const PostItem = ({ 
+  addLike, 
+  removeLike, 
+  deletePost, 
+  auth, 
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions
 }) => <div class="post bg-white p-1 my-1">
     <div>
       <Link to="profile.html">
@@ -23,7 +29,9 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
        <p class="post-date">
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-      <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
+
+      {showActions && <Fragment>
+        <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
         <i class="fas fa-thumbs-up"></i>{' '}
         <span>{likes.length > 0 && (
           <span>{likes.length}</span>
@@ -46,6 +54,7 @@ const PostItem = ({ addLike, removeLike, deletePost, auth, post: { _id, text, na
       class="btn btn-danger"
     ></button>
       )}
+        </Fragment>}
     </div> 
   </div>
 
