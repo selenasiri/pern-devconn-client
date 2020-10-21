f72ddcebda3f282cb4b5b6f4f6f0786023eeda77
Selena Siri <selenasiri@gmail.com>
2020-10-21 00:49:59 -0400

    commentitem posts

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,31 +1,43 @@
-import React, { Fragment, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import Spinner from '../layout/Spinner'
-import PostItem from '../posts/PostItem'
-import { getPost } from '../../actions/post'
-import {useParams} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
+import { getPost } from '../../actions/post';
 
-// /posts/:id    --- e.g. /posts/2
+// path="/posts/:id" in src/routing/Routes.js
 const Post = () => {
-const {post, loading} = useSelector(state => state.post)
-const dispatch = useDispatch()
+  const { post, loading } = useSelector(state => state.post)
+  const dispatch = useDispatch()
 
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
-  const { id } = useParams(); // id = 2
+  const { id } = useParams();
 
   useEffect(() => {
     dispatch(getPost(id));
   }, [getPost, id]);
 
-  return loading || post === null ? <Spinner /> : 
-  <Fragment>
-    <Link to='/posts' className='btn'> 
-     Back To Posts
-    </Link>
-    <PostItem post={post} showActions={false} /> 
-  </Fragment>
-}
+  if (loading || post === null) {
+    return <Spinner />
+  }
 
-export default Post
+  return (
+    <>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Post;
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,31 +1,32 @@
-import React, { Fragment, useEffect } from 'react'
-import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner'
-import PostItem from './PostItem'
-import PostForm from './PostForm'
-import { getPost } from '../../actions/post';
-import CommentForm from '../post/CommentForm';
-import post from '../../reducers/post';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
+import { getPosts } from '../../actions/post';
 
+const Posts = () => {
+  const { posts } = useSelector(state => state.post)
+  const dispatch = useDispatch()
 
-const Posts = ({ getPosts, post: { posts, loading }}) => {
   useEffect(() => {
-    getPosts();
+    dispatch(getPosts());
   }, [getPosts]);
 
-  return loading ? <Spinner /> : <Fragment>test
-  <h1 className="large text-primary">Posts</h1>
-   <p className="lead">
-    <i className="fas fa-user">Welcome to the community</i>
-  </p>
-  <PostForm />
-  <div>
-    {posts.map(post => (
-      <PostItem key={post._id} post={post} />
-    ))}
-  </div>
-  <CommentForm postId={post._id} /> 
-  </Fragment>;
+  return (
+    <>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      <PostForm />
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Posts
+export default Posts;
+
